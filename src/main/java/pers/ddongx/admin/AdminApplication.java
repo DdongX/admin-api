80084934b65e5c0e1424af847373287673db6872
DdongX <2510589885@qq.com>
2022-12-04 16:35:57 +0800

    feature 自定义ID生成器

--- src/main/java/pers/ddongx/admin/AdminApplication.java
+++ src/main/java/pers/ddongx/admin/AdminApplication.java
@@ -1,9 +1,15 @@
 package pers.ddongx.admin;
 
+import com.github.yitter.contract.IdGeneratorOptions;
+import com.github.yitter.idgen.YitIdHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pers.ddongx.admin.util.LogUtil;
 
+/**
+ * @author DdongX
+ * @since 2022/12/3
+ */
 @SpringBootApplication
 public class AdminApplication {
 
@@ -11,6 +17,16 @@ public class AdminApplication {
         try {
             SpringApplication app = new SpringApplication(AdminApplication.class);
             app.run(args);
+            // 创建 IdGeneratorOptions 对象，可在构造函数中输入 WorkerId：
+            IdGeneratorOptions options = new IdGeneratorOptions();
+            // options.WorkerIdBitLength = 10; // 默认值6，限定 WorkerId 最大值为2^6-1，即默认最多支持64个节点。
+            // options.SeqBitLength = 6; // 默认值6，限制每毫秒生成的ID个数。若生成速度超过5万个/秒，建议加大 SeqBitLength 到 10。
+            // options.BaseTime = Your_Base_Time; // 如果要兼容老系统的雪花算法，此处应设置为老系统的BaseTime。
+            // ...... 其它参数参考 IdGeneratorOptions 定义。
+            // 保存参数（务必调用，否则参数设置不生效）：
+            YitIdHelper.setIdGenerator(options);
+            // 以上过程只需全局一次，且应在生成ID之前完成。
+
             LogUtil.info("AdminApplication App Start !!!!!!");
         } catch (Exception e) {
             LogUtil.error("AdminApplication App Start Failed !!!!!!!!", e);
--- src/main/java/pers/ddongx/admin/config/MybatisPlusConfiguration.java
+++ src/main/java/pers/ddongx/admin/config/MybatisPlusConfiguration.java
@@ -1,7 +1,9 @@
 package pers.ddongx.admin.config;
 
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusPropertiesCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.github.yitter.idgen.YitIdHelper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +15,14 @@ import org.springframework.context.annotation.Configuration;
 public class MybatisPlusConfiguration {
 
     @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
         final MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         return interceptor;
+    }
+
+    @Bean
+    public MybatisPlusPropertiesCustomizer plusPropertiesCustomizer() {
+        return plusProperties -> plusProperties.getGlobalConfig().setIdentifierGenerator(entity -> YitIdHelper.nextId());
     }
 }
