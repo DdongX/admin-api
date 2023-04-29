bd310a06d974fdb3ecc344723e73c55875e971eb
DdongX <2510589885@qq.com>
2023-04-29 23:33:55 +0800

    bug 修复knife4j登陆不生效问题

--- src/main/java/pers/ddongx/admin/config/SaTokenConfig.java
+++ src/main/java/pers/ddongx/admin/config/SaTokenConfig.java
@@ -17,6 +17,6 @@ public class SaTokenConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册 Sa-Token 拦截器，打开注解式鉴权功能
-        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin())).addPathPatterns("/**");
+        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin())).addPathPatterns("/**").excludePathPatterns("/swagger-ui.html", "/swagger-ui/**", "/doc.html", "/v3/api-docs/**", "/webjars/js/**", "/webjars/css/**");
     }
 }
